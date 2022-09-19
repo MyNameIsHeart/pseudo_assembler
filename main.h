d838a687acd6c14b22d00a5f24b4867d7e2b8840
exodus <noamgithub@protonmail.com>
2022-09-19 18:02:53 +0300

    removing source code files

--- main.h
+++
@@ -1,7 +0,0 @@
-/* header to main.c */
-
-#include "libs.h"           /* include C standard libraries */
-#include "exit_codes.h"     /* include exit codes header */
-
-/* functions in use from first_pass.c */
-int first_pass(char *); /* main sends the files to the assembler by calling first_pass() */
